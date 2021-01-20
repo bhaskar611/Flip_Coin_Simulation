4ff32755951aaa9936bec4ef31d2560f1b84b8b0
unknown <bhaskarnavuluri9@gmail.com>
2021-01-20 10:14:26 +0530

    modified uc2

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -1,24 +1,31 @@
-#!/bin/bash
-	read -p "Enter no of times to flip: " n
-	headscount=0
-	tailscount=0
-	for((i = 1; i <= n; i++ ))
-	do
-	    flip=$((RANDOM%2))
-	    if(( flip == 0 ))
-	    then
-	        headscount=$(($headscount + 1))
-	    else
-	        tailscount=$(($tailscount + 1 ))
-	    fi 
-	done
-	echo "Heads count is $headscount and Tails Count is $tailscount"
-	if(( $headscount > $tailscount ))
-	then
-	    echo "Winner is Heads"
-	elif(($tailscount > $headscount ))
-	then
-	    echo "Winner is Tails"
-	else    
-	    echo "Tie"
-	fi
+#! /bin/bash
+count=21
+headscount=0
+tailscount=0
+flipscount=0
+while(( 1 ))
+do
+    flipscount=$(($flipscount + 1))
+    flip=$((RANDOM%2))
+    if((flip == 0 ))
+    then
+        headscount=$(($headscount + 1 ))
+    else
+        tailscount=$(($tailscount + 1 ))
+    fi
+    if(( $headscount == $count || $tailscount == $count ))
+    then
+        break
+    fi 
+done
+echo "Heads count is" $headscount "and Tails Count is" $tailscount
+if(( $headscount > $tailscount ))
+then
+    echo "Heads won by" $(( $headscount - $tailscount ))
+elif(($tailscount > $headscount))
+then
+    echo "Tails won by" $(( $tailscount - $headscount ))
+else    
+    echo "Tie"
+fi
+
