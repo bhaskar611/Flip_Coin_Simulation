349563f37163df3b83b444f384f00b8b8fcebbba
unknown <bhaskarnavuluri9@gmail.com>
2021-01-20 09:36:03 +0530

    Displays Multiple Times Head or Tail wins

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -1,9 +1,24 @@
-#! /bin/bash
-
-flip=$((RANDOM%2))
-if (($flip == 0))
-then
-    echo "head Wins"
-else
-    echo "Tail Wins"
-fi
+#!/bin/bash
+	read -p "Enter no of times to flip: " n
+	headscount=0
+	tailscount=0
+	for((i = 1; i <= n; i++ ))
+	do
+	    flip=$((RANDOM%2))
+	    if(( flip == 0 ))
+	    then
+	        headscount=$(($headscount + 1))
+	    else
+	        tailscount=$(($tailscount + 1 ))
+	    fi 
+	done
+	echo "Heads count is $headscount and Tails Count is $tailscount"
+	if(( $headscount > $tailscount ))
+	then
+	    echo "Winner is Heads"
+	elif(($tailscount > $headscount ))
+	then
+	    echo "Winner is Tails"
+	else    
+	    echo "Tie"
+	fi
